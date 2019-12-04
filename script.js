47858d1f88d26d6fc05c8f099fc82efc7823df40
arseniyepti <ars-us@yandex.ru>
2019-12-04 16:24:36 +0300

    4
    4

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 "use strict";
 
 let brandsButton = document.querySelector('#brands-button');
-let brandsHidden = document.querySelector('.brands__images-wrap');
-
-let count = function () {
-  let result;
-  let arr = document.querySelectorAll('.brands__images-item');
-  if (screen.width > 1119) {
-    result = arr.length - 8;
-  } else {
-    result = arr.length - 6;
+let brandsWrap = document.querySelector('.brands__images-wrap');
+let arrBrands = document.querySelectorAll('.brands__images-item');
+let arrBrandsButtons = document.querySelectorAll('.brands__button');
+brandsButton.textContent = `Показать все (${arrBrands.length})`;
+let setTabIndex = function () {
+  for (let i = 0; i < arrBrands.length; i++) {
+    if (brandsWrap.classList.contains('brands__images-wrap-hidden')) {
+      if (screen.width > 1119 && i > 7) {
+        arrBrandsButtons[i].setAttribute('tabindex', '-1');
+      }
+      if (screen.width > 768 && screen.width < 1120 && i > 5) {
+        arrBrandsButtons[i].setAttribute('tabindex', '-1');
+      }
+      if (screen.width < 768 && i > 1) {
+        arrBrandsButtons[i].setAttribute('tabindex', '-1');
+      }
+    }
   }
-  return brandsButton.textContent = 'Показать все' + `(${result})`;
 }
-
-let count1 = count();
+setTabIndex();
 
 brandsButton.addEventListener('click', function (evt) {
   evt.preventDefault();
-  brandsHidden.classList.toggle('brands__images-wrap-hidden');
-  let brandsButtonClassName = brandsHidden.className;
-  if (brandsButtonClassName !== 'brands__images - wrap brands__images - wrap - hidden') {
+  brandsWrap.classList.toggle('brands__images-wrap-hidden');
+  if (!brandsWrap.classList.contains('brands__images-wrap-hidden')) {
     brandsButton.textContent = 'Скрыть';
-  } else { count1; }
+  } else {
+    brandsButton.textContent = `Показать все (${arrBrands.length})`;
+  }
 })
